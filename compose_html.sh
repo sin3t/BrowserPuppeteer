8d67f95f48867b624f7fbfb2676ca02e62baa42c
sin3t <sin3t.dev@gmail.com>
2016-10-10 21:10:13 +0300

    Initial commit. Added docs generators

---
+++ compose_html.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Usage
+# 1. Generate html with styles into generated-docs.html
+#   bash compose_html.sh > generated-docs.html
+# a2. Commit changes to current branch
+# a3. Switch to main branch (development), and sync documentation versions
+#   OR
+# b2. Copy changes files directrly in main branch -> NOT Recommended
+CSS_FILE="implied_style.css"
+HTML_TEMPLATE_FILE="html_struct_template.txt"
+rst2htmlCMD="rst2html2 -v --template=$HTML_TEMPLATE_FILE"
+
+read -r -d '' STYLE <<- EOP
+	<style type='text/css'>
+	$(cat $CSS_FILE)
+	</style>
+EOP
+
+echo "$STYLE"
+cat ../web/docs/COMMANDS.md \
+    | sed -n '/^## Actions list$/,/^## /{/^## \|```\|^###/!p;}' \
+    | awk -v genhtmlcom="$rst2htmlCMD" \
+        'BEGIN \
+        {print "<h3> Actions list </h3>";
+         print "<ul>";
+         FS="\n";
+         RS="";
+         counter=0;}
+            {rstTable=$0;
+             accesskey=substr($2,8,1);
+             actionDescr=substr($2,7,1)"<u>"accesskey"</u>"substr($2,9,35);
+             print "<li>";
+             print "<input type=checkbox id=item-"counter" oninput=\"!this.checked || this.scrollIntoView()\">";
+             print "<label for=item-"counter"></label>";
+             print "<label for=item-"counter" class=data accesskey=\""accesskey"\"> "actionDescr" </label>";
+             print "<div>";
+             cmd=genhtmlcom" << EOF\n"rstTable"\nEOF";
+             system(cmd);
+             close(cmd);
+             print "</div>";
+             print "</li>"
+             counter++;}
+        END \
+        {print "</ul>"}'
